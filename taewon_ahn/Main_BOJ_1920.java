6ebe214ddadca5af702f9fa68aa16641508d2244
Taewon Ahn <withdandy09@naver.com>
2025-08-09 23:31:50 +0900

    [BOJ] 수 찾기 / 실버 4 / 5분
    https://www.acmicpc.net/problem/1920

---
+++ taewon_ahn/Main_BOJ_1920.java
@@ -0,0 +1,44 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main_BOJ_1920 {
+    static int N, M;
+    static int[] A;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+        M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            binarySearch(target);
+        }
+    }
+
+    private static void binarySearch(int target) {
+        int start = 0;
+        int end = N - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (A[mid] > target) {
+                end = mid - 1;
+            } else if (A[mid] < target) {
+                start = mid + 1;
+            } else {
+                System.out.println(1);
+                return;
+            }
+        }
+        System.out.println(0);
+    }
+
+}
