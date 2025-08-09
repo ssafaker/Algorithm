3db46748548be10cc51b5fe3f41a22b1370927c0
Taewon Ahn <withdandy09@naver.com>
2025-08-09 09:04:51 +0900

    [BOJ] 최솟값 찾기 / 플래티넘 5 / 50분
    https://www.acmicpc.net/problem/11003

--- taewon_ahn/Main_BOJ_11003.java
+++ taewon_ahn/Main_BOJ_11003.java
@@ -1,4 +1,43 @@
 package taewon_ahn;
 
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
 public class Main_BOJ_11003 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        Deque<Node> deque = new ArrayDeque<>();
+        for (int i = 0; i < N; i++) {
+            Node curNode = new Node(i, Integer.parseInt(st.nextToken()));
+            // 이전 모든 노드가 현재 노드보다 크다면 모두 제거
+            while (!deque.isEmpty() && deque.getLast().value > curNode.value) {
+                deque.removeLast();
+            }
+            // 현재 노드 추가
+            deque.addLast(curNode);
+            // 첫번째 노드가 범위 안인지 확인
+            if (deque.getFirst().index <= i - L) {
+                deque.removeFirst();
+            }
+            // 첫번째 노드 출력
+            sb.append(deque.getFirst().value).append(" ");
+        }
+        System.out.print(sb);
+    }
+
+    static class Node {
+        int value, index;
+
+        Node(int index, int value) {
+            this.index = index;
+            this.value = value;
+        }
+    }
 }
