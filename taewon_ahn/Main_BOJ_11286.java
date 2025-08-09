7a8f3edfe82f1df4d5916c08bc80670948258b3c
Taewon Ahn <withdandy09@naver.com>
2025-08-09 09:11:11 +0900

    [BOJ] 절댓값 힙 / 실버 1 / 30분
    https://www.acmicpc.net/problem/11286

--- taewon_ahn/Main_BOJ_11286.java
+++ taewon_ahn/Main_BOJ_11286.java
@@ -1,10 +1,11 @@
 package taewon_ahn;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
 public class Main_BOJ_11286 {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
