b56ca449835636899b873fc5d0a5e44010a372e4
Taewon Ahn <withdandy09@naver.com>
2025-08-16 21:22:08 +0900

    [SEA] 디저트 카페 / 모의 SW 역량테스트 / 50분
    https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5VwAr6APYDFAWu

--- taewon_ahn/Main_SEA_2105.java
+++ taewon_ahn/Main_SEA_2105.java
@@ -30,14 +30,7 @@ public class Main_SEA_2105 {
                     startR = r;
                     startC = c;
                     visited[map[r][c]] = true;
-                    int nr = r + dr[0];
-                    int nc = c + dc[0];
-                    if (check(nr, nc) && !visited[map[nr][nc]]) {
-                        // 첫 시작은 무조건 0방향 직선 출발
-                        visited[map[nr][nc]] = true;
-                        dfs(nr, nc, 0, 1);
-                        visited[map[nr][nc]] = false;
-                    }
+                    dfs(r, c, 0, 1);
                     visited[map[r][c]] = false;
                 }
             }
@@ -47,21 +40,19 @@ public class Main_SEA_2105 {
     }
 
     private static void dfs(int r, int c, int d, int cnt) {
-        for (int nd = d; nd <= d + 1; nd++) { // 직선 또는 우측 꺽기
-            if (nd >= 4)
-                return;
-
+        for (int nd = d; nd < 4; nd++) { // 직선 또는 우측 꺽기
             int nr = r + dr[nd];
             int nc = c + dc[nd];
 
-            if (nd == 3 && nr == startR && nc == startC) {
-                if (cnt + 1 > maxCnt) {
-                    maxCnt = cnt + 1;
-                }
+            if (!check(nr, nc))
                 continue;
+
+            if (nr == startR && nc == startC && cnt >= 4) {
+                maxCnt = Math.max(maxCnt, cnt);
+                return;
             }
 
-            if (check(nr, nc) && !visited[map[nr][nc]]) {
+            if (!visited[map[nr][nc]]) {
                 visited[map[nr][nc]] = true;
                 dfs(nr, nc, nd, cnt + 1);
                 visited[map[nr][nc]] = false;
