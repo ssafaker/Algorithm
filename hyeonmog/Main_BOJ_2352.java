6a4bdd5912c36e9c772c8fc70ca00a1cfea28557
KimHyeonMog <dmo2595@naver.com>
2025-09-03 14:38:29 +0900

    [BOJ] 반도체설계 / 골드2 / 40분
    https://www.acmicpc.net/problem/2352

--- hyeonmog/Main_BOJ_2352.java
+++ hyeonmog/Main_BOJ_2352.java
@@ -8,14 +8,13 @@ import java.util.StringTokenizer;
 class Main_BOJ_2352 {
     static int n;
     static int[] arr;
-    static int[] increaseDp, decreaseDp;
+    static int[] increaseDp;
     static int result;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         arr = new int[n+1];
         increaseDp = new int[n+1];
-        decreaseDp = new int[n+1];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 1 ; i < n+1 ; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
@@ -24,17 +23,12 @@ class Main_BOJ_2352 {
 
         for(int k = 1 ; k < n+1 ; k++) {
             increaseDp[k] = 1;
-            decreaseDp[k] = 1;
             for(int i = 1 ; i < k ; i++) {
-                if(arr[k] < arr[i] && k < i) {
+                if(arr[k] > arr[i]) {
                     increaseDp[k] = Math.max(increaseDp[k], increaseDp[i]+1);
-                }
-                if(arr[k] > arr[i] && k > i) {
-                    decreaseDp[k] = Math.max(decreaseDp[k], decreaseDp[i]+1);
                 }
             }
             result = Math.max(result, increaseDp[k]);
-            result = Math.max(result, decreaseDp[k]);
         }
 
         System.out.println(result);
